9e8d8a4b157c6e0bfcafc821d2d0b6706246966b
weishaodaren <zhaojiawei@startable.cn>
2023-08-12 10:40:10 +0800

    feat: style

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,51 +1,21 @@
-import type { PropsWithChildren, FC, CSSProperties } from 'react';
+import type { PropsWithChildren, FC } from 'react';
 
 import { Layout as AntdLayout, Space } from 'antd';
 
 const { Header, Sider, Content, Footer } = AntdLayout;
-
-const headerStyle: CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 64,
-  paddingInline: 50,
-  lineHeight: '64px',
-  backgroundColor: '#7dbcea',
-};
-
-const contentStyle: CSSProperties = {
-  textAlign: 'center',
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#108ee9',
-};
-
-const siderStyle: CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#3ba0e9',
-};
-
-const footerStyle: CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#7dbcea',
-};
 
 /**
  * Layout
  */
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Space direction="vertical" style={{ width: '100%', height: '100%' }} size={[0, 48]}>
+    <Space direction="vertical" className="w-full h-full" size={[0, 48]}>
       <AntdLayout>
-        <Sider style={siderStyle}>Sider</Sider>
+        <Sider className="text-center h-10 bg-cyan-700 text-sky-50">Sider</Sider>
         <AntdLayout>
-          <Header style={headerStyle}>Header</Header>
-          <Content style={contentStyle}>{children}</Content>
-          <Footer style={footerStyle}>Footer</Footer>
+          <Header className="text-center h-10 bg-cyan-500 text-sky-200">Header</Header>
+          <Content className="text-center h-10 bg-cyan-300 text-sky-400">{children}</Content>
+          <Footer className="text-center h-10 bg-cyan-100 text-sky-600">Footer</Footer>
         </AntdLayout>
       </AntdLayout>
     </Space>
