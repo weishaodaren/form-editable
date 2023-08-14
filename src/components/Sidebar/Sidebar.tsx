aa1a0a1a1365a4d27957141f7f78115cd3d132fb
weishaodaren <zhaojiawei@startable.cn>
2023-08-14 18:56:43 +0800

    feat: update avatar url

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,6 +5,8 @@ import { widgetsConfig } from '@/shared';
 
 const { Meta } = Card;
 
+const avatarUrl = new URL('~/public/avatar.png', import.meta.url).href;
+
 /**
  * 侧边栏
  */
@@ -13,11 +15,7 @@ const Sidebar = () => {
     <div key={widget.title} className=" w-60 h-auto">
       <HoverCard>
         <Card cover={<img src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" alt="cover" />}>
-          <Meta
-            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-            title={widget.title}
-            description="this is weisahodaren"
-          />
+          <Meta avatar={<Avatar src={avatarUrl} />} title={widget.title} description={`${widget.title} widget`} />
         </Card>
       </HoverCard>
     </div>
